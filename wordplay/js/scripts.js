3c2379dc610fd0a21a1f852012324ea1f102f726
jin camou and sailor winkelman <sailor@>
2016-10-03 22:34:17 -0700

    Add .split() method to separate user inputted sentence into words in an array

--- wordplay/js/scripts.js
+++ wordplay/js/scripts.js
@@ -1,17 +1,9 @@
-
-
 $(document).ready(function() {
   $("form#blanks").submit(function() {
     event.preventDefault();
-    var sentence = $("input#sentence").val();
-    var splitSentence = [];
-    sentence.split(" ");
-    
-    console.log(sentence);
+    var words = $("input#sentence").val().split(" ");
+    var wordLength = [];
     $("#output").text(sentence);
-    /*sentence.forEach(function(word) {
-      var userInput = $("input#" + word).val();
-      splitSentence.push(userInput);
-    });*/
+
   });
 });
