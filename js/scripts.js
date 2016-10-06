61a1ad339d4d4ec70187f65852a764da130e0cf5
jin camou and sailor winkelman <jin+sailor@>
2016-10-05 18:00:08 -0700

    Remove wordLength variable and edit js function to resolve issue where if condition is never met.

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,11 @@ $(document).ready(function() {
     event.preventDefault();
     var words = $("input#sentence").val().split(" ");
     var newArray = [];
-    var wordLength = words.map(function(word) {
-      return word.length;
-    });
     console.log(words);
-    console.log(wordLength);
-
-//figure out why this section doesn't ever meet the condition!
+    
     words.forEach(function(word) {
-      if (length === 3) {
-        $('#title').hide();
-        } else {
-        $('#blanks').hide();
+      if (word.length <= 3) {
+        newArray.push(word);
         }
     });
     console.log(newArray);
